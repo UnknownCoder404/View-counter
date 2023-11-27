9693cd9aa3c981a47d0da90c556be8db886b4552
Jakov C <qwerasdftzu@gmail.com>
2023-11-27 13:24:01 +0100

    Change interval time

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 
-const intervalTime = 60 * 1000;
+const intervalTime = 3600 * 1000; // 3600 seconds
 let viewCount = 0;
 const app = express();
 console.log(`Viewcount: ${viewCount}`);

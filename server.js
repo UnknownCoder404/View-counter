9f2445f7d64c4f999f6b29d9d107a908ecb0e601
Jakov C <qwerasdftzu@gmail.com>
2023-12-03 16:49:06 +0100

    Add online-saving

--- server.js
+++ server.js
@@ -1,37 +1,81 @@
-require("dotenv").config();
+// Import the required modules
 const express = require("express");
-const http = require("http");
-let { LocalStorage } = require("node-localstorage");
-localStorage = new LocalStorage("./localstorage");
-let viewCount = Number(localStorage.getItem("viewCount")) || 0;
-saveToLocal();
-const intervalTime = 3600 * 1000; // 3600 seconds
+const mongoose = require("mongoose");
+const cron = require("node-cron");
+const dotenv = require("dotenv");
+
+// Load the environment variables from the .env file
+dotenv.config();
+
+// Create an express app
 const app = express();
-console.log(`Viewcount: ${viewCount}`);
-app.post("/add-view", (req, res) => {
-  viewCount += 1;
-  console.log(`Viewcount: ${viewCount}`);
-  saveToLocal();
-  res.json({
-    message: "Thank you for visiting my site",
-  });
+
+// Connect to MongoDB Atlas cloud database using the mongoURI from the .env file
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-setInterval(() => {
-  loadToLocal();
-  console.log(`Viewcount: ${viewCount}`);
-}, intervalTime);
-const server = http.createServer(app);
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+// Define a schema for the viewCount document
+const viewCountSchema = new mongoose.Schema({
+  count: Number,
 });
 
-function saveToLocal() {
-  localStorage.setItem("viewCount", viewCount);
-}
-function loadToLocal() {
-  localStorage = Number(localStorage.getItem("viewCount"));
-}
+// Create a model for the viewCount collection
+const ViewCount = mongoose.model("ViewCount", viewCountSchema);
+
+// Create a middleware to parse JSON requests
+app.use(express.json());
+
+// Create a route handler for /add-view POST requests
+app.post("/add-view", async (req, res) => {
+  try {
+    // Find the viewCount document in the database
+    let viewCount = await ViewCount.findOne();
+
+    // If it does not exist, create a new one with count 0
+    if (!viewCount) {
+      viewCount = new ViewCount({ count: 0 });
+    }
+
+    // Increment the count by 1
+    viewCount.count++;
+
+    // Save the updated document to the database
+    await viewCount.save();
+    console.log(`Added viewcount: ${viewCount.count}`);
+    // Send a success response with the current count
+    res
+      .status(200)
+      .json({ message: "View count updated", count: viewCount.count });
+  } catch (error) {
+    // If there is an error, send a failure response with the error message
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: error.message });
+  }
+});
+
+// Create a cron job that runs every hour and logs the viewCount to the console
+cron.schedule("0 * * * *", async () => {
+  try {
+    // Find the viewCount document in the database
+    let viewCount = await ViewCount.findOne();
+
+    // If it exists, log the count to the console
+    if (viewCount) {
+      console.log(`The view count is ${viewCount.count}`);
+    } else {
+      // If it does not exist, log a message to the console
+      console.log("The view count is not initialized");
+    }
+  } catch (error) {
+    // If there is an error, log it to the console
+    console.error(error);
+  }
+});
+
+// Start the server on the port from the .env file
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});

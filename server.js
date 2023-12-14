2e909079081831d42fbf4687b1dd94713378e93e
Jakov C <qwerasdftzu@gmail.com>
2023-12-14 19:21:49 +0100

    Add logging

--- server.js
+++ server.js
@@ -1,97 +1,81 @@
-// Import the required modules
+// Import modules
 const express = require("express");
 const mongoose = require("mongoose");
-const cron = require("node-cron");
-const dotenv = require("dotenv");
-const router = express.Router();
 const cors = require("cors");
-
-// CORS
+require("dotenv").config();
 const corsOptions = {
-  origin: ["http://localhost:5500", "https://odvjetnik.josip.cicek.hr"],
+  origin: [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://odvjetnik.josip.cicek.hr",
+    "https://odvjetnik.josip.cicek.hr",
+  ],
   methods: ["POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
-router.use(cors(corsOptions));
+cors(corsOptions);
+// Connect to MongoDB database using mongoose
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database.");
+  })
+  .catch((err) => console.error(err));
 
-// Load the environment variables from the .env file
-dotenv.config();
-
-// Create an express app
-const app = express();
-
-// Connect to MongoDB Atlas cloud database using the mongoURI from the .env file
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Define a schema for the viewCount document
-const viewCountSchema = new mongoose.Schema({
+// Define a schema and a model for the log collection
+const logSchema = new mongoose.Schema({
+  item: String,
   count: Number,
 });
+const Log = mongoose.model("Log", logSchema);
 
-// Create a model for the viewCount collection
-const ViewCount = mongoose.model("ViewCount", viewCountSchema);
+// Create an express app and a router
+const app = express();
+const router = express.Router();
 
-// Create a middleware to parse JSON requests
-app.use(express.json());
+// Define an array of items
+const items = [
+  "helpful",
+  "not-helpful",
+  "google-maps",
+  "telephone",
+  "email",
+  "view",
+];
 
-// Create a route handler for /add-view POST requests
-app.post("/add-view", async (req, res) => {
-  try {
-    // Find the viewCount document in the database
-    let viewCount = await ViewCount.findOne();
-
-    // If it does not exist, create a new one with count 0
-    if (!viewCount) {
-      viewCount = new ViewCount({ count: 0 });
-    }
-
-    // Increment the count by 1
-    viewCount.count++;
-
-    // Save the updated document to the database
-    await viewCount.save();
-    console.log(`Added viewcount: ${viewCount.count}`);
-    // Send a success response with the current count
-    res
-      .status(200)
-      .json({ message: "View count updated", count: viewCount.count });
-  } catch (error) {
-    // If there is an error, send a failure response with the error message
-    res
-      .status(500)
-      .json({ message: "Something went wrong", error: error.message });
+items.forEach(async (item) => {
+  let tempLog = await Log.findOne({ item });
+  if (!tempLog) {
+    tempLog = new Log({ item, count: 0 });
   }
+  console.log(`Current state of ${item}: ${tempLog.count}`);
 });
-
-// Create a cron job that runs every hour and logs the viewCount to the console
-cron.schedule("0 * * * *", async () => {
-  try {
-    // Find the viewCount document in the database
-    let viewCount = await ViewCount.findOne();
-
-    // If it exists, log the count to the console
-    if (viewCount) {
-      console.log(`The view count is ${viewCount.count}`);
-    } else {
-      // If it does not exist, log a message to the console
-      console.log("The view count is not initialized");
+// Create a dynamic route for each item
+items.forEach((item) => {
+  router.post(`/add-log/${item}`, async (req, res) => {
+    // Find or create a log document for the item
+    let log = await Log.findOne({ item });
+    if (!log) {
+      log = new Log({ item, count: 0 });
     }
-  } catch (error) {
-    // If there is an error, log it to the console
-    console.error(error);
-  }
+    // Increment the count and save the log
+    log.count++;
+    await log.save();
+    // Send a response
+    console.log(`Added a log for ${item}. Count is now ${log.count}.`);
+    res.send(`Added a log for ${item}. Count is now ${log.count}.`);
+  });
 });
-
 app.post("/restart-server", (req, res) => {
   console.log("Server restarted.");
   res.status(200).json({ message: "Server restarted" });
 });
-
-// Start the server on the port from the .env file
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+app.use(router);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
